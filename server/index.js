46673be23d977b8de7eada4108bb96f90a86b8f8
jjpeso <your-email@example.com>
2019-06-20 19:11:21 -0700

    actual server side rendering probably

--- server/index.js
+++ server/index.js
@@ -1,43 +1,49 @@
-import express from 'express';
+import path from 'path'
+import fs from 'fs'
 
-const PORT = 3000;
-const path = require('path');
-const fs = require("fs");
-
+import express from 'express'
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
-import { StaticRouter } from 'react-router-dom';
+import { StaticRouter } from 'react-router-dom'
 
-import App from '../src/App.tsx';
+import App from '../src/App.tsx'
 
-const app = express();
+const PORT = 3000
+const app = express()
 
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+const router = express.Router()
 
-app.get('*', (req, res, next) => {
-  const filePath = path.resolve(__dirname, '..', 'build', 'index.html');
-  
-  fs.readFile(filePath, 'utf8', (err, htmlData) => {
+const serverRenderer = (req, res, next) => {
+  fs.readFile(path.resolve('./build/index.html'), 'utf8', (err, data) => {
     if (err) {
-      console.error('err', err);
-      return res.status(404).end()
+      console.error(err)
+      return res.status(500).send('An error occurred')
     }
-
-    const html = ReactDOMServer.renderToString(
+    
+    var html = ReactDOMServer.renderToString(
       <StaticRouter location={req.url}>
         <App />
       </StaticRouter>
     );
-    
+
     return res.send(
-      htmlData.replace(
+      data.replace(
         '<div id="root"></div>',
         `<div id="root">${html}</div>`
       )
-    );
-  });
-});
+    )
+  })
+}
 
+router.use(
+  express.static(path.resolve(__dirname, '..', 'build'), { maxAge: '30d' })
+)
+router.use('/', serverRenderer)
+
+// tell the app to use the above rules
+app.use(router)
+
+// app.use(express.static('./build'))
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+  console.log(`SSR running on port ${PORT}`)
 })
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import './styles/index.scss';
-
 import * as serviceWorker from './serviceWorker';
 
 import App from './App';
